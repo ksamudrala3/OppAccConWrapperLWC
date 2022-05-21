6bd8d8d9de4cf7e2a1706da9c65ac8729e490044
ksamudrala3 <krishntej1@gmail.com>
2022-05-21 21:10:06 +0530

    Used reusable Datatable Component

---
+++ force-app/main/default/lwc/dataTableCmp/dataTableCmp.js
@@ -0,0 +1,32 @@
+import { LightningElement, api, track, wire } from 'lwc';
+
+export default class DataTableCmp extends LightningElement {
+    @api columns;
+    @api opportunities;
+
+    @track sortBy;
+    @track sortDirection;
+    doSorting(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        let parseData = JSON.parse(JSON.stringify(this.opportunities));
+        // Return the value stored in the field
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+        // cheking reverse direction
+        let isReverse = direction === 'asc' ? 1: -1;
+        // sorting data
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+        this.opportunities = parseData;
+    }  
+}
