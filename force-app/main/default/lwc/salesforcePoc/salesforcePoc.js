1fb4a8e4488e7bcab09eeba354c8194040d717fb
ksamudrala3 <krishntej1@gmail.com>
2022-05-22 12:10:46 +0530

    Adding pagination

--- force-app/main/default/lwc/salesforcePoc/salesforcePoc.js
+++ force-app/main/default/lwc/salesforcePoc/salesforcePoc.js
@@ -93,15 +93,11 @@ export default class SalesforcePoc extends LightningElement {
     stagevalue;
     accValue;
     conValue;
-    stageNameValue;
+    stageNameValue = '';
     showFooter;
-    numRecoptions = [{ label: 5, value: 5 },
-    { label: 10, value: 10 },
-        { label: 50, value: 50 }];
-    numOfRec = 5;
+    limitRecs = 1000;
     @track oppStages = [];
     @track error;
-    @track opportunities = [];
     @track columns = columns;
     @track sortBy;
     @track sortDirection;
@@ -123,10 +119,19 @@ export default class SalesforcePoc extends LightningElement {
         }
         
         getOppRecords({
-            StageName: this.stagevalue, AccountName: this.accValue, ContactName: this.conValue, limitNum:this.numOfRec})
-            .then((result) => {                
-                this.opportunities = result;
-                this.showFooter = this.opportunities.length > 0 ? true : false;
+            StageName: this.stagevalue, AccountName: this.accValue, ContactName: this.conValue, limitNum:this.limitRecs})
+            .then((data) => {  
+                this.items = data;
+                this.totalRecountCount = data.length; //here it is 23
+                this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); //here it is 5
+                
+                //initial data to be displayed ----------->
+                //slice will take 0th element and ends with 5, but it doesn't include 5th element
+                //so 0 to 4th rows will be displayed in the table
+                this.data = this.items.slice(0,this.pageSize); 
+                this.endingRecord = this.pageSize;
+                this.columns = columns;
+                this.showFooter = this.totalRecountCount > 0 ? true : false;
                 this.error = undefined;
             })
             .catch((error) => {
@@ -135,6 +140,55 @@ export default class SalesforcePoc extends LightningElement {
                 this.opportunities = undefined;
             });
     }
+
+    // Pagination start
+
+    @track page = 1; //this will initialize 1st page
+    @track items = []; //it contains all the records.
+    @track data = []; //data to be displayed in the table
+    @track startingRecord = 1; //start record position per page
+    @track endingRecord = 0; //end record position per page
+    @track pageSize = 5; //default value we are assigning
+    @track totalRecountCount = 0; //total record count received from all retrieved records
+    @track totalPage = 0; //total number of page is needed to display all records
+
+    previousHandler() {
+        if (this.page > 1) {
+            this.page = this.page - 1; //decrease page by 1
+            this.displayRecordPerPage(this.page);
+        }
+    }
+
+    //clicking on next button this method will be called
+    nextHandler() {
+        if((this.page<this.totalPage) && this.page !== this.totalPage){
+            this.page = this.page + 1; //increase page by 1
+            this.displayRecordPerPage(this.page);            
+        }             
+    }
+
+    displayRecordPerPage(page){
+
+        /*let's say for 2nd page, it will be => "Displaying 6 to 10 of 23 records. Page 2 of 5"
+        page = 2; pageSize = 5; startingRecord = 5, endingRecord = 10
+        so, slice(5,10) will give 5th to 9th records.
+        */
+        this.startingRecord = ((page -1) * this.pageSize) ;
+        this.endingRecord = (this.pageSize * page);
+
+        this.endingRecord = (this.endingRecord > this.totalRecountCount) 
+                            ? this.totalRecountCount : this.endingRecord; 
+
+        this.data = this.items.slice(this.startingRecord, this.endingRecord);
+
+        //increment by 1 to display the startingRecord count, 
+        //so for 2nd page, it will show "Displaying 6 to 10 of 23 records. Page 2 of 5"
+        this.startingRecord = this.startingRecord + 1;
+    }
+
+    // Pagination end
+
+        // Column sorting start
 
     doSorting(event) {
         this.sortBy = event.detail.fieldName;
@@ -159,4 +213,6 @@ export default class SalesforcePoc extends LightningElement {
         });
         this.opportunities = parseData;
     }  
+
+     // Column sorting end
 }
